a20e53b01f26d1dbb731e55ff4c753ea09bfc7f3
Samuel Garcia <sam.garcia.cortes@gmail.com>
2023-09-19 07:34:34 -0700

    updates

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from io import TextIOWrapper
 from typing import TextIO
 
 import OIE
+import tutorials
 import utility_functions as util_func
 
 import numpy as np
@@ -42,57 +43,11 @@ from wordcloud import WordCloud
 # OIE_CORPUS_FOLDER = r"C:\Users\Sam\Downloads\snapshot_oie_corpus.tar\snapshot_oie_corpus\oie_corpus"
 
 
-def coreNLP_tutorial(text: str = None, corpus_path: str = None):
-    from openie import StanfordOpenIE
-
-    # Demo purposes
-    if text is None:
-        text = 'Barack Obama was born in Hawaii. Richard Manning wrote this sentence.'
-    if corpus_path is None:
-        corpus_path = 'corpus/pg6130.txt'
-
-    # https://stanfordnlp.github.io/CoreNLP/openie.html#api
-    # Default value of openie.affinity_probability_cap was 1/3.
-    properties = {
-        'openie.affinity_probability_cap': 2 / 3,
-    }
-
-    with StanfordOpenIE(properties=properties) as client:
-        # text = 'Barack Obama was born in Hawaii. Richard Manning wrote this sentence.'
-        print('Text: %s.' % text)
-        for triple in client.annotate(text):
-            print('|-', triple)
-
-        # graph_image = 'graph.png'
-        # client.generate_graphviz_graph(text, graph_image)
-        # print('Graph generated: %s.' % graph_image)
-
-        with open(corpus_path, encoding='utf8') as r:
-            corpus = r.read().replace('\n', ' ').replace('\r', '')
-
-        triples_corpus = client.annotate(corpus[0:5000])
-        print('Corpus: %s [...].' % corpus[0:80])
-        print('Found %s triples in the corpus.' % len(triples_corpus))
-        for triple in triples_corpus[:5]:
-            print('|-', triple)
-        print('[...]')
-
-
 def main():
-    # corpus_path: str = "OIE Sentences.txt"
-    # text: str = "My name is Samuel Garcia. I was born in California. I wrote this sentence."
-    # coreNLP_tutorial(text=text, corpus_path=corpus_path)
-
-    OIE.run_oie_analysis()
-
-    # from allennlp.predictors.predictor import Predictor
-    # import allennlp_models.tagging
-    #
-    # predictor = Predictor.from_path(
-    #     "https://storage.googleapis.com/allennlp-public-models/openie-model.2020.03.26.tar.gz")
-    # predictor.predict(
-    #     sentence="In December, John decided to join the party.."
-    # )
+    corpus_path: str = "OIE Sentences Original.txt"
+    text: str = "My name is Samuel Garcia. I was born in California. I wrote this sentence."
+    tutorials.coreNLP_tutorial(text=text, corpus_path=corpus_path)
+    # OIE.run_oie_analysis()
 
 
 if __name__ == '__main__':
--- tutorials.py
+++ tutorials.py
@@ -775,3 +775,49 @@ def samson_zhang_nn():
 
     for i in range(5):
         test_prediction(i, W1, b1, W2, b2)
+
+# def allenlp_tutorial():
+#     from allennlp.predictors.predictor import Predictor
+#     import allennlp_models.tagging
+#
+#     predictor = Predictor.from_path(
+#         "https://storage.googleapis.com/allennlp-public-models/openie-model.2020.03.26.tar.gz")
+#     predictor.predict(
+#         sentence="In December, John decided to join the party.."
+#     )
+
+
+def coreNLP_tutorial(text: str = None, corpus_path: str = None):
+    from openie import StanfordOpenIE
+
+    # Demo purposes
+    if text is None:
+        text = 'Barack Obama was born in Hawaii. Richard Manning wrote this sentence.'
+    if corpus_path is None:
+        corpus_path = 'corpus/pg6130.txt'
+
+    # https://stanfordnlp.github.io/CoreNLP/openie.html#api
+    # Default value of openie.affinity_probability_cap was 1/3.
+    properties = {
+        'openie.affinity_probability_cap': 2 / 3,
+    }
+
+    with StanfordOpenIE(properties=properties) as client:
+        # text = 'Barack Obama was born in Hawaii. Richard Manning wrote this sentence.'
+        print('Text: %s.' % text)
+        for triple in client.annotate(text):
+            print('|-', triple)
+
+        # graph_image = 'graph.png'
+        # client.generate_graphviz_graph(text, graph_image)
+        # print('Graph generated: %s.' % graph_image)
+
+        with open(corpus_path, encoding='utf8') as r:
+            corpus = r.read().replace('\n', ' ').replace('\r', '')
+
+        triples_corpus = client.annotate(corpus[0:5000])
+        print('Corpus: %s [...].' % corpus[0:80])
+        print('Found %s triples in the corpus.' % len(triples_corpus))
+        for triple in triples_corpus[:5]:
+            print('|-', triple)
+        print('[...]')
