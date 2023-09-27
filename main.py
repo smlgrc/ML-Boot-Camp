5f2d3beb4df881e73d5a0c1910ae402c3dab0b88
Samuel Garcia <sam.garcia.cortes@gmail.com>
2023-09-27 13:42:26 -0700

    started info extraction tutorial

--- main.py
+++ main.py
@@ -43,11 +43,91 @@ from wordcloud import WordCloud
 # OIE_CORPUS_FOLDER = r"C:\Users\Sam\Downloads\snapshot_oie_corpus.tar\snapshot_oie_corpus\oie_corpus"
 
 
+import subprocess
+
+def spacy_claucy():
+    import spacy
+    import claucy
+
+    nlp = spacy.load("en_core_web_sm")
+    claucy.add_to_pipe(nlp)
+
+    doc = nlp("AE died in Princeton in 1955.")
+
+    print(doc._.clauses)
+    # Output:
+    # &lt;SV, AE, died, None, None, None, [in Princeton, in 1955]&gt;
+
+    propositions = doc._.clauses[0].to_propositions(as_text=True)
+
+    breakpoint()
+
+    print(propositions)
+    # Output:
+    # [AE died in Princeton in 1955, AE died in 1955, AE died in Princeton
+
+
+def ollie_tutorial():
+    sentence = "Barack Obama was born in Hawaii."
+    try:
+        # Run Ollie as a subprocess
+        result = subprocess.run(
+            ["java", "-Xmx1g", "-jar", "ollie/ollie-app-latest.jar", "corpus/sample.txt"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+
+        print("here1")
+
+        # Extracted information is in the standard output
+        output = result.stdout
+
+        print("here2")
+
+        # Split the output into lines and extract relevant information
+        lines = output.strip().split("\n")
+
+        breakpoint()
+
+        subject = lines[0]
+        predicate = lines[1]
+        arguments = lines[2:]
+
+        print("here3")
+
+        print(subject, predicate, arguments)
+        return subject, predicate, arguments
+    except Exception as e:
+        print("Error:", str(e))
+        return None, None, []
+
+
+def huggingface():
+    from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+    tokenizer = AutoTokenizer.from_pretrained("XLab/rst-information-extraction-11b")
+    model = AutoModelForSeq2SeqLM.from_pretrained("XLab/rst-information-extraction-11b")
+
+    inputs = tokenizer.encode(
+        "TEXT: this is the best cast iron skillet you will ever buy. QUERY: Is this review \"positive\" or \"negative\"",
+        return_tensors="pt")
+    outputs = model.generate(inputs)
+    print(tokenizer.decode(outputs[0], skip_special_tokens=True, clean_up_tokenization_spaces=True))
+
+
 def main():
-    corpus_path: str = "OIE Sentences Original.txt"
-    text: str = "My name is Samuel Garcia. I was born in California. I wrote this sentence."
-    tutorials.coreNLP_tutorial(text=text, corpus_path=corpus_path)
+    # corpus_path: str = "OIE Sentences Original.txt"
+    # text: str = "My name is Samuel Garcia. I was born in California. I wrote this sentence."
+    # tutorials.coreNLP_tutorial(text=text, corpus_path=corpus_path)
+
     # OIE.run_oie_analysis()
+
+    # spacy_claucy()
+    # huggingface()
+    ollie_tutorial()
+
+
 
 
 if __name__ == '__main__':
---
+++ rest_api_demo2.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = ""
+
+    if request.method == "POST":
+        sentence = request.form.get("sentence")
+        # Perform some processing or analysis on the sentence here
+        result = process_sentence(sentence)
+
+    return render_template("index.html", result=result)
+
+
+def process_sentence(sentence):
+    # Replace this with your sentence processing logic
+    return "You entered: " + sentence
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
